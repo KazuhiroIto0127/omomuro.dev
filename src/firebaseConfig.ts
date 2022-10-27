caf0f7f05b06a544bcb04a58f9411a58c68fa4d2
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2022-10-28 00:28:01 +0900

    vue-gtagを追加。vue-routerに対応。

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check"
 import { getFirestore } from 'firebase/firestore'
-// import { getAnalytics } from "firebase/analytics";
 
 const config = {
   apiKey: "AIzaSyBGSnz95KrcyW5w6YO8p_iJAfalGb8Jr_4",
@@ -10,8 +9,6 @@ const config = {
   storageBucket: "omomuro-dev.appspot.com",
   messagingSenderId: "676406123871",
   appId: "1:676406123871:web:18e24f922e5772ddc02aea",
-  measurementId: "G-7J8CT114L4"
-
 }
 
 if (import.meta.env.DEV) {
@@ -19,7 +16,6 @@ if (import.meta.env.DEV) {
   window.self.FIREBASE_APPCHECK_DEBUG_TOKEN = true // デバッグ用文字列の生成
 }
 const app = initializeApp(config)
-// const analytics = getAnalytics(app);
 
 // App Checkでクライアントをチェックする
 const appCheck = initializeAppCheck(app, {
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
+import VueGtag from "vue-gtag";
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -10,5 +11,8 @@ library.add(faUserSecret,faFlask)
 
 const app = createApp(App)
 app.use(router)
+if (import.meta.env.DEV) {
+  app.use(VueGtag, {config: { id: "G-YBD2S57910" }}, router)
+}
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
