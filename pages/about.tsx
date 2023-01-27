69940d44f78e5ce281cb7810f27ddf526749a50c
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-01-28 00:09:05 +0900

    aboutページをgridでいい感じにした

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,8 @@
 import Layout from '@/components/layouts/oneColumnLayout';
 import Image from 'next/image';
 import HeadMeta from '@/components/Head';
-import { AcademicCapIcon, CommandLineIcon } from '@heroicons/react/24/outline';
+import { TwitterLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons';
+import { AcademicCapIcon, CommandLineIcon, LinkIcon } from '@heroicons/react/24/outline';
 import {
   Ruby,
   Rubyonrails,
@@ -40,34 +41,42 @@ export default function About() {
     <Layout>
       <HeadMeta type="website" title="自分のこと" />
 
-      <div className="mx-auto max-w-2xl">
-        <h1 className="mb-2">自分のこと</h1>
-        <div
-          className="flex h-40 w-full items-center justify-center rounded-lg bg-slate-100 dark:bg-slate-600"
-          style={{
-            backgroundImage: `url(
+      <h1 className="mb-2">自分のこと</h1>
+      <div className="grid-col-1 md:grid-col-2 lg:grid-col-3 md:grid-row-5 grid gap-4 md:grid-flow-col">
+        <section className="row-span-2">
+          <div
+            className="grid grid-cols-2 place-items-center rounded-lg bg-slate-100 p-3 dark:bg-slate-600"
+            style={{
+              backgroundImage: `url(
               "data:image/svg+xml,%3Csvg width='80' height='80' viewBox='0 0 80 80' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.19'%3E%3Cpath d='M50 50c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10s-10-4.477-10-10 4.477-10 10-10zM10 10c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10S0 25.523 0 20s4.477-10 10-10zm10 8c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm40 40c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E"
             )`,
-          }}
-        >
-          <Image priority src="/images/omo_pen.png" height={100} width={100} alt="logo" />
-        </div>
+            }}
+          >
+            <Image priority src="/images/omo_pen.png" height={100} width={100} alt="logo" />
+            <div>
+              <p className="mx-auto my-3 block text-xl font-bold">イトウ カズヒロ</p>
+              <p className="mb-2 leading-7">
+                1990年1月27日 生まれ
+                <br />
+                大分県出身、東京都在住
+                <br />
+                エンジニア
+                <br />
+                趣味はイラスト、プログラミング
+              </p>
+            </div>
+          </div>
 
-        <section className="mb-12">
-          <p className="mx-auto my-3 block text-xl font-bold">イトウ カズヒロ</p>
-          <p className="mb-2 leading-7">
-            1990年1月27日に生まれる
-            <br />
-            大分県出身、東京都在住
-            <br />
-            仕事はエンジニア
-            <br />
-            イラスト、プログラミングが趣味
+          <p className="px-3 pt-3 leading-7">
+            はじめまして！こんにちは。
+            <br /> こんなところまで見て頂きとても嬉しいです！
+            <br /> このomomuro.devは、私のポートフォリオのようなサイトになります。
+            <br /> 技術的な実験を試みたりもするので、たまに見るとサイトが変わってたりするかもです！
           </p>
         </section>
 
         <section
-          className="mb-12 rounded-lg bg-slate-100 p-4 dark:bg-slate-600"
+          className="rounded-lg bg-slate-100 p-4 dark:bg-slate-600 md:row-span-2 md:row-start-3 md:row-end-5"
           style={{
             backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.1'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E"
           )`,
@@ -149,7 +158,7 @@ export default function About() {
           </ol>
         </section>
 
-        <section className="mb-12 rounded-lg bg-gray-100 p-4 dark:bg-slate-800">
+        <section className="rounded-lg bg-gray-100 p-4 dark:bg-slate-600 md:col-start-2 md:row-span-3">
           <p className="mb-4 flex items-center">
             <CommandLineIcon className="mr-2 inline w-6" />
             <span>プログラミングについて</span>
@@ -157,140 +166,171 @@ export default function About() {
 
           <div className="mb-10">
             <p className="mb-4 w-fit border-b-2 border-gray-300">業務で使ったことがある技術、言語</p>
-            <ul className="flex flex-wrap gap-8">
+            <ul className="grid grid-flow-row grid-cols-5 gap-8">
               <li className="flex flex-col items-center justify-center">
                 <Ruby width={30} height={30} className="mb-2 text-red-500" />
-                <span className="text-sm text-gray-500">Ruby</span>
+                <span className="text-sm text-gray-400">Ruby</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Rubyonrails width={30} height={30} className="mb-2 text-red-500" />
-                <span className="text-sm text-gray-500">Rails</span>
+                <span className="text-sm text-gray-400">Rails</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Nuxtdotjs width={30} height={30} className="mb-2 text-green-500" />
-                <span className="text-sm text-gray-500">Nuxt.js</span>
+                <span className="text-sm text-gray-400">Nuxt.js</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Vuedotjs width={30} height={30} className="mb-2 text-green-500" />
-                <span className="text-sm text-gray-500">Vue.js</span>
+                <span className="text-sm text-gray-400">Vue.js</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Typescript width={30} height={30} className="mb-2 text-blue-500" />
-                <span className="text-sm text-gray-500">Typescript</span>
+                <span className="text-sm text-gray-400">Typescript</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Javascript width={30} height={30} className="mb-2 text-yellow-500" />
-                <span className="text-sm text-gray-500">Javascript</span>
+                <span className="text-sm text-gray-400">Javascript</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Html5 width={30} height={30} className="mb-2 text-red-500" />
-                <span className="text-sm text-gray-500">Html5</span>
+                <span className="text-sm text-gray-400">Html5</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <CssThree width={30} height={30} className="mb-2 text-blue-500" />
-                <span className="text-sm text-gray-500">CSS3</span>
+                <span className="text-sm text-gray-400">CSS3</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Mysql width={30} height={30} className="mb-2" />
-                <span className="text-sm text-gray-500">Mysql</span>
+                <span className="text-sm text-gray-400">Mysql</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Postgresql width={30} height={30} className="mb-2" />
-                <span className="text-sm text-gray-500">Postgresql</span>
+                <span className="text-sm text-gray-400">Postgresql</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Redis width={30} height={30} className="mb-2 text-red-500" />
-                <span className="text-sm text-gray-500">Redis</span>
+                <span className="text-sm text-gray-400">Redis</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Amazonaws width={30} height={30} className="mb-2" />
-                <span className="text-sm text-gray-500">AWS</span>
+                <span className="text-sm text-gray-400">AWS</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Chartdotjs width={30} height={30} className="mb-2 text-pink-300" />
-                <span className="text-sm text-gray-500">Chart.js</span>
+                <span className="text-sm text-gray-400">Chart.js</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Docker width={30} height={30} className="mb-2 text-cyan-400" />
-                <span className="text-sm text-gray-500">Docker</span>
+                <span className="text-sm text-gray-400">Docker</span>
               </li>
             </ul>
           </div>
 
           <div className="mb-10">
             <p className="mb-4 w-fit border-b-2 border-gray-300">趣味で使った技術、言語</p>
-            <ul className="flex flex-wrap gap-8">
+            <ul className="grid grid-flow-row grid-cols-5 gap-8">
               <li className="flex flex-col items-center justify-center">
                 <ReactJs width={30} height={30} className="mb-2 text-blue-500" />
-                <span className="text-sm text-gray-500">React.js</span>
+                <span className="text-sm text-gray-400">React.js</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Tailwindcss width={30} height={30} className="mb-2 text-blue-200" />
-                <span className="text-sm text-gray-500">Tailwindcss</span>
+                <span className="text-sm text-gray-400">Tailwindcss</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Gatsby width={30} height={30} className="mb-2 text-purple-500" />
-                <span className="text-sm text-gray-500">Gatsby</span>
+                <span className="text-sm text-gray-400">Gatsby</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Nextdotjs width={30} height={30} className="mb-2" />
-                <span className="text-sm text-gray-500">Next.js</span>
+                <span className="text-sm text-gray-400">Next.js</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Firebase width={30} height={30} className="mb-2 text-yellow-500" />
-                <span className="text-sm text-gray-500">Firebase</span>
+                <span className="text-sm text-gray-400">Firebase</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Graphql width={30} height={30} className="mb-2 text-pink-500" />
-                <span className="text-sm text-gray-500">Graphql</span>
+                <span className="text-sm text-gray-400">Graphql</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Swift width={30} height={30} className="mb-2 text-orange-500" />
-                <span className="text-sm text-gray-500">Swift</span>
+                <span className="text-sm text-gray-400">Swift</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Php width={30} height={30} className="mb-2 text-purple-300" />
-                <span className="text-sm text-gray-500">PHP</span>
+                <span className="text-sm text-gray-400">PHP</span>
               </li>
             </ul>
           </div>
 
           <div className="mb-10">
             <p className="mb-4 w-fit border-b-2 border-gray-300">開発環境、ツールたち</p>
-            <ul className="flex flex-wrap gap-8">
+            <ul className="grid grid-flow-row grid-cols-5 gap-8">
               <li className="flex flex-col items-center justify-center">
                 <Neovim width={30} height={30} className="mb-2 text-green-500" />
-                <span className="text-sm text-gray-500">Neovim</span>
+                <span className="text-sm text-gray-400">Neovim</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Visualstudiocode width={30} height={30} className="mb-2 text-blue-500" />
-                <span className="text-sm text-gray-500">VS Code</span>
+                <span className="text-sm text-gray-400">VS Code</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <ItermTwo width={30} height={30} className="mb-2" />
-                <span className="text-sm text-gray-500">Iterm2</span>
+                <span className="text-sm text-gray-400">Iterm2</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Figma width={30} height={30} className="mb-2 text-red-300" />
-                <span className="text-sm text-gray-500">Figma</span>
+                <span className="text-sm text-gray-400">Figma</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Postman width={30} height={30} className="mb-2 text-orange-500" />
-                <span className="text-sm text-gray-500">Postman</span>
+                <span className="text-sm text-gray-400">Postman</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Slack width={30} height={30} className="mb-2 text-purple-400" />
-                <span className="text-sm text-gray-500">Slack</span>
+                <span className="text-sm text-gray-400">Slack</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Github width={30} height={30} className="mb-2" />
-                <span className="text-sm text-gray-500">Github</span>
+                <span className="text-sm text-gray-400">Github</span>
               </li>
               <li className="flex flex-col items-center justify-center">
                 <Apple width={30} height={30} className="mb-2" />
-                <span className="text-sm text-gray-500">Apple</span>
+                <span className="text-sm text-gray-400">Apple</span>
               </li>
             </ul>
+          </div>
+        </section>
+
+        <section className="rounded-lg bg-gray-100 p-4 dark:bg-slate-600 md:col-start-2 md:row-span-1 md:row-start-1 lg:col-start-3">
+          <p className="mb-4 flex items-center">
+            <LinkIcon className="mr-2 inline w-6" />
+            <span>SNSリンク</span>
+          </p>
+          <div className="grid grid-cols-3 place-items-center gap-5">
+            <a
+              href="https://twitter.com/KazuhiroIto0127"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col items-center justify-center text-sky-400"
+            >
+              <TwitterLogoIcon className="h-6 w-6" /> Twitter
+            </a>
+            <a
+              href="https://github.com/KazuhiroIto0127"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col items-center justify-center text-sky-400"
+            >
+              <GitHubLogoIcon className="h-6 w-6" /> Github
+            </a>
+            <a href="https://zenn.dev/kazuhiroito0127" target="_blank" rel="noreferrer" className="block text-sky-400">
+              Zenn
+            </a>
+            <a href="https://qiita.com/KazuhiroIto0127" target="_blank" rel="noreferrer" className="block text-sky-400">
+              Qiita
+            </a>
           </div>
         </section>
       </div>
