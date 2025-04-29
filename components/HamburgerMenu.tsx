6b427934bc7c4187a7fc926463f923bf43313c43
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-04-29 21:58:11 +0900

    down grade tailwaindcss version and fix error components/HamburgerMenu.tsx

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -37,34 +37,37 @@ export const HamburgerMenu = () => {
         <Bars3Icon className="w-6" />
       </button>
 
-      <Dialog as="div" className="fixed inset-0 z-50" open={isOpen} onClose={() => setIsOpen(false)}>
-        <Dialog.Overlay className="fixed inset-0 bg-gray-800 bg-opacity-70 backdrop-blur-sm" />
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="fixed inset-0 z-50">
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-70 backdrop-blur-sm" />
 
-        <Dialog.Panel className="dark:highlight-white/5 absolute top-4 right-4 w-full max-w-xs rounded-lg bg-white p-6 text-base font-semibold text-slate-900 shadow-lg dark:bg-slate-800 dark:text-slate-400">
-          <button type="button" className="absolute top-5 right-5 h-8 w-8" onClick={() => setIsOpen(false)}>
-            <XMarkIcon className="h-5 w-5" />
-          </button>
+        <div className="fixed inset-0 flex items-center justify-center">
+          <Dialog.Panel className="dark:highlight-white/5 w-full max-w-xs rounded-lg bg-white p-6 text-base font-semibold text-slate-900 shadow-lg dark:bg-slate-800 dark:text-slate-400">
+            <button type="button" className="absolute top-5 right-5 h-8 w-8" onClick={() => setIsOpen(false)}>
+              <XMarkIcon className="h-5 w-5" />
+            </button>
 
-          <nav className="">
-            <ul className="space-y-6">
-              {links.map((link) => (
-                <li key={link.path}>
-                  <Link
-                    className={clsx('hover:text-sky-500', {
-                      'text-sky-500': currentRoute === link.path,
-                    })}
-                    href={link.path}
-                  >
-                    {link.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <div className="mt-6 border-t border-slate-200 pt-6 dark:border-slate-200/10">
-            <ThemeChangerRadioBtn />
-          </div>
-        </Dialog.Panel>
+            <nav className="">
+              <ul className="space-y-6">
+                {links.map((link) => (
+                  <li key={link.path}>
+                    <Link
+                      className={clsx('hover:text-sky-500', {
+                        'text-sky-500': currentRoute === link.path,
+                      })}
+                      href={link.path}
+                      onClick={() => setIsOpen(false)}
+                    >
+                      {link.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <div className="mt-6 border-t border-slate-200 pt-6 dark:border-slate-200/10">
+              <ThemeChangerRadioBtn />
+            </div>
+          </Dialog.Panel>
+        </div>
       </Dialog>
     </>
   );
