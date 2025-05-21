0fdd4f2af1e6c9b10416d24cacc946b9d07f265d
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2025-05-21 21:08:08 +0900

    update book shelf style

--- pages/books.tsx
+++ pages/books.tsx
@@ -26,18 +26,13 @@ export default function Books({ books }: { books: Book[] }) {
   return (
     <Layout>
       <h1 className="mb-8 text-3xl font-bold">読んだ本一覧</h1>
-      <div className="relative bg-[#8B4513] p-8 rounded-lg shadow-2xl">
-        <div
-          className="absolute inset-0 rounded-lg opacity-80"
-          style={{
-            backgroundImage: "url('/images/wood-texture.jpg')",
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        ></div>
+      <div className="relative bg-white/50 dark:bg-black/50 p-8 rounded-lg shadow-2xl">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-12 md:gap-y-20 max-w-[1000px] mx-auto relative">
           {books.map((book, index) => (
-            <div key={book.slug} className="relative">
+            <div
+              key={book.slug}
+              className="relative after:content-[''] after:absolute after:-left-4 after:-right-4 after:bottom-0 after:h-4 after:bg-[#eaeaea] dark:after:bg-[#515151] after:rounded-b-lg after:shadow-inner"
+            >
               <div className="transform hover:-translate-y-2 transition-all duration-300">
                 <Link href={`/books/${book.slug}`} className="focus:outline-none w-full max-w-[212px] block mx-auto">
                   <div className="relative">
@@ -49,11 +44,6 @@ export default function Books({ books }: { books: Book[] }) {
                     <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/20 rounded"></div>
                   </div>
                 </Link>
-              </div>
-              {/* 棚板 */}
-              <div className="relative mt-4">
-                <div className="absolute -left-4 -right-4 h-4 bg-[#6B3410] rounded-b-lg shadow-inner"></div>
-                <div className="absolute -left-4 -right-4 h-1 bg-[#8B4513] top-1"></div>
               </div>
             </div>
           ))}
